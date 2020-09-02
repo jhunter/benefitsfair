ab9e9493308d52958e8b7902dfe1a78c50b68898
jhunter <jhunter@gmail.com>
2020-09-02 16:48:42 -0500

    no message

---
+++ wp-content/themes/benefitsfair/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        pkl: {
+          '100': '#fcf5ff',
+          '200': '#f2d6ff',
+          '300': '#e6b7ff',
+          '400': '#cf8aff',
+          '500': '#b968fd',
+          '600': '#9c45ea',
+          '700': '#8632d6',
+          '800': '#682daa',
+          '900': '#522886',
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+    require('@tailwindcss/custom-forms'),
+  ],
+}
